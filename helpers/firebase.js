4f425c0fde4512eb59c43f07dd5395c328ed596f
Elcid-Chawa <elcidchawa@gmail.com>
2025-01-03 01:48:10 +0200

    fixed firebase config

--- helpers/firebase.js
+++ helpers/firebase.js
@@ -2,14 +2,14 @@
 const admin = require("firebase-admin");
 require("dotenv").config();
 // const path = require("path");
-var serviceAccount = require("../serviceAccountKey.json");
+// var serviceAccount = require("../serviceAccountKey.json");
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseconfig = {
+const serviceAccount = {
   type: "service_account",
   // apiKey: process.env.FIREBASE_API_KEY,
   // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
